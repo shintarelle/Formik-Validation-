6349e04db3a547f1458c43095098099d3ea1fa19
AnnRozhkina <annrozhkina@gmail.com>
2023-02-02 00:53:23 +0200

    create Form with formik and validation with yup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
+import React from 'react';
 import './App.css';
+import RegistrationForm from './components/RegistrationForm';
+
 
 function App() {
   return (
     <>
-
+      <h1>Registration Form with Formik and Yup</h1>
+      <div className="form-wrapper">
+        <RegistrationForm />
+      </div>
     </>
   );
 }
